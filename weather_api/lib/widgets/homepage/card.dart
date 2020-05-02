3edbf0cc6b7221d616a991cdf58e612d7c17d704
Davi Emediato <48991554+daviemediato@users.noreply.github.com>
2020-05-02 00:45:43 -0300

    delete button

--- weather_api/lib/widgets/homepage/card.dart
+++ weather_api/lib/widgets/homepage/card.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:weather_api/model/city.dart';
 import 'package:weather_api/pages/weatherpage.dart';
 import 'package:weather_api/structures/constants.dart';
+import 'package:weather_api/structures/helper.dart';
 
-class CardTile extends StatelessWidget {
+class CardTile extends StatefulWidget {
   final List<City> cities;
   final int index;
 
   CardTile(this.cities, this.index);
+
+  @override
+  _CardTileState createState() => _CardTileState();
+}
+
+class _CardTileState extends State<CardTile> {
+  final Helper helper = new Helper();
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +31,7 @@ class CardTile extends StatelessWidget {
           color: kSecondaryColor,
         ),
         title: Text(
-          cities[index].name,
+          widget.cities[widget.index].name,
           style: TextStyle(
             color: kSecondaryColor,
             fontWeight: FontWeight.bold,
@@ -31,7 +39,7 @@ class CardTile extends StatelessWidget {
           ),
         ),
         subtitle: Text(
-          cities[index].country,
+          widget.cities[widget.index].country,
           style: TextStyle(
             color: kSecondaryColor,
             fontWeight: FontWeight.bold,
@@ -41,17 +49,76 @@ class CardTile extends StatelessWidget {
         trailing: IconButton(
           icon: Icon(Icons.drag_handle),
           color: kSecondaryColor,
-          onPressed: () {},
+          onPressed: () {
+            _showDrag(context);
+          },
         ),
         onTap: () async {
           await Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => WeatherPage(name: cities[index].name),
+              builder: (context) =>
+                  WeatherPage(name: widget.cities[widget.index].name),
             ),
           );
         },
       ),
+    );
+  }
+
+  void _showDrag(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: kPrimaryColor,
+          title: Text(
+            'Hmm...deleting?',
+            style: TextStyle(
+              color: Colors.yellow,
+            ),
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  'This is annoying message from this stupid weather app :/.',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                'Delete',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                helper.deleteCity(widget.cities[widget.index].id);
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
